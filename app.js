b809266c700657de959fb34878d6b978439148fe
subirdas29 <dasjoy104@gmail.com>
2022-08-23 12:25:59 +0600

    js value

---
+++ app.js
@@ -0,0 +1,13 @@
+// Initializing value
+const perPlayerBudget = document.getElementById('per-player-budget');
+const playerExpenses = document.getElementById('playerExpenses');
+
+const managerBudget = document.getElementById('manager-budget');
+
+const coachBudget = document.getElementById('coach-budget');
+
+const totalExpence = document.getElementById('total-cost');
+
+document.getElementById('total-calculate').style.opacity = "0.2";
+
+
